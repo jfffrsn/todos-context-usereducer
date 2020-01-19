ce2eeef7e2a4081e9f5cbe967f82fa807cf59589
jeffferson99 <49041684+jeffferson99@users.noreply.github.com>
2020-01-18 22:59:38 -0500

    reducer added

--- src/TodoForm.js
+++ src/TodoForm.js
@@ -5,22 +5,22 @@ import useInputState from "./hooks/useInputState";
 import { TodosContext } from "./context/todos.context";
 function TodoForm() {
   const [value, handleChange, reset] = useInputState("");
-  const { addTodo } = useContext(TodosContext);
+  const { dispatch } = useContext(TodosContext);
   console.log("TODO FORM RENDER!!!");
   return (
     <Paper style={{ margin: "1rem 0", padding: "0 1rem" }}>
       <form
         onSubmit={e => {
           e.preventDefault();
-          addTodo(value);
+          dispatch({ type: "ADD", task: value });
           reset();
         }}
       >
         <TextField
           value={value}
           onChange={handleChange}
-          margin='normal'
-          label='Add New Todo'
+          margin="normal"
+          label="Add New Todo"
           fullWidth
         />
       </form>
--- src/context/todos.context.js
+++ src/context/todos.context.js
@@ -1,5 +1,6 @@
-import React, { createContext } from "react";
-import useTodoState from "../hooks/useTodoState";
+import React, { createContext, useReducer } from "react";
+import todoReducer from "../reducer/todo.reducer";
+//import useTodoState from "../hooks/useTodoState";
 const defaultTodos = [
   { id: 1, task: "Mow the lawn using goats", completed: false },
   { id: 2, task: "Release lady bugs into garden", completed: true }
@@ -7,9 +8,9 @@ const defaultTodos = [
 export const TodosContext = createContext();
 
 export function TodosProvider(props) {
-  const todosStuff = useTodoState(defaultTodos);
+  const [todos, dispatch] = useReducer(todoReducer, defaultTodos);
   return (
-    <TodosContext.Provider value={todosStuff}>
+    <TodosContext.Provider value={{ todos, dispatch }}>
       {props.children}
     </TodosContext.Provider>
   );
