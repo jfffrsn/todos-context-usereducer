0671d6a51afcb65b6616713999e44e2f5f338fee
jeffferson99 <49041684+jeffferson99@users.noreply.github.com>
2020-01-19 01:17:24 -0500

    Updates

---
+++ precache-manifest.24b4a1e5a177220a6cdd28dcbd5d251e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "13f529cab154ec02dc0f856342a69644",
+    "url": "/todos-context-usereducer/index.html"
+  },
+  {
+    "revision": "698887f54602f1193ccd",
+    "url": "/todos-context-usereducer/static/css/main.462b0bd9.chunk.css"
+  },
+  {
+    "revision": "c189f9392acade90fef4",
+    "url": "/todos-context-usereducer/static/js/2.7e1a41d1.chunk.js"
+  },
+  {
+    "revision": "2e7d2c37866ca6c066224e205e1b0aa3",
+    "url": "/todos-context-usereducer/static/js/2.7e1a41d1.chunk.js.LICENSE"
+  },
+  {
+    "revision": "698887f54602f1193ccd",
+    "url": "/todos-context-usereducer/static/js/main.f4e4c144.chunk.js"
+  },
+  {
+    "revision": "0e7b6ca065ed3e2cf1d7",
+    "url": "/todos-context-usereducer/static/js/runtime-main.90f5acc6.js"
+  }
+]);
